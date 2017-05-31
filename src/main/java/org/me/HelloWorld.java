6480ae6ab08d0148c2213868c4bafff5ae025094
kiracookie <kira0109@qq.com>
2017-05-31 16:35:53 +0800

    demo

---
+++ src/main/java/org/me/HelloWorld.java
@@ -0,0 +1,29 @@
+package org.me;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Controller
+public class HelloWorld {
+
+    /**
+     * 1. 使用RequestMapping注解来映射请求的URL
+     * 2. 返回值会通过视图解析器解析为实际的物理视图, 对于InternalResourceViewResolver视图解析器，会做如下解析
+     * 通过prefix+returnVal+suffix 这样的方式得到实际的物理视图，然后会转发操作
+     * "/WEB-INF/views/success.jsp"
+     *
+     * @return
+     */
+    @RequestMapping("/helloworld")
+    @ResponseBody
+    public Map<String, Object> hello() {
+        System.out.println("hello world");
+        Map<String, Object> a = new LinkedHashMap<>();
+        a.put("cc", 21312);
+        return a;
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kai.zang
+  Date: 2017/5/31
+  Time: 15:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>$Title$</title>
+</head>
+<body>
+123123123ccc
+</body>
+</html>
